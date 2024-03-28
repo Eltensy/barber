9434ed8d244a91dc78789ac314d41bdc42b089af
RostykStets <ste2806murosl@gmail.com>
2024-03-28 20:17:49 +0200

    Add DTOs

---
+++ BuinessLogicLayer/DTOs/ClientDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuinessLogicLayer.DTOs
+{
+    public class ClientDto
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Surname { get; set; }
+        public required string Phone { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+    }
+}
---
+++ BuinessLogicLayer/DTOs/VisitDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuinessLogicLayer.DTOs
+{
+    public class VisitDto
+    {
+        public int Id { get; set; }
+        public int? fk_CLientId { get; set; }
+        public int? fk_GuestId { get; set; } 
+        public int fk_BarberId { get; set; } 
+        public int fk_ServiceId { get; set; }
+        public DateOnly Date {  get; set; }
+        public TimeOnly Time {  get; set; }
+    }
+}
